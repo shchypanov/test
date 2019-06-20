0be7fa33773092047cfad6519b176b5c73341cf4
vlad shchypanov <vladshchypanov@gmail.com>
2019-06-20 21:04:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const student = {
+    firstname: 'Glad',
+    lastname: 'Chinda',
+    country: 'Nigeria'
+};
+// Деструктурированный объект
+const { firstname, lastname, country } = student;
+console.log(firstname, lastname, country);
